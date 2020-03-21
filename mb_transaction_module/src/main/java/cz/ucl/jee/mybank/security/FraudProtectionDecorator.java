351bb1bac8d3c2c026e37957d4ec2e6e1f4fb8d2
Ondrej Fialka <ondrej.fialka@unicorncollege.cz>
2020-03-21 15:18:08 +0100

    implemented decorator

--- mb_transaction_module/src/main/java/cz/ucl/jee/mybank/security/FraudProtectionDecorator.java
+++ mb_transaction_module/src/main/java/cz/ucl/jee/mybank/security/FraudProtectionDecorator.java
@@ -4,26 +4,30 @@ import java.math.BigDecimal;
 
 import javax.decorator.Decorator;
 import javax.decorator.Delegate;
-import javax.enterprise.inject.Any;
 import javax.inject.Inject;
 
 import cz.ucl.jee.mybank.accounts.AccountBlackList;
 import cz.ucl.jee.mybank.entity.PaymentOrder;
 import cz.ucl.jee.mybank.transfer.OrderSender;
 
-//TODO Annotate to make this class a decorator and declare it to implement OrderSender
-public class FraudProtectionDecorator {
+@Decorator
+public class FraudProtectionDecorator implements OrderSender {
 
 	public static BigDecimal MAX_AMOUNT = new BigDecimal(1000000);
 
-	//TODO Annotate
+	@Delegate
+	@Inject
 	private OrderSender orderSender;
 	
-	//TODO Inject AccountBlackList
+	@Inject
+	private AccountBlackList accountBlackList;
 
 	public void sendPaymentOrder(PaymentOrder order) {
 
-		//TODO implmenent blacklist checking
+		if (accountBlackList.isAccountOnList(order.getCreditAccount())) {
+			System.out.println("Account on blacklist!");
+			return;
+		}
 
 		if (order.getAmount().compareTo(MAX_AMOUNT) > 0){
 			System.out.println("Amount too large, transaction denied.");
